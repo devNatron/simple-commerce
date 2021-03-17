79886b4ba1121fa09710bda7a2a5b474fb8dbe00
natron <vitornovaes.santos@gmail.com>
2021-03-17 19:47:34 -0300

    serverless nodemailer function created

---
+++ serverless/mailer.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  auth:{
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+})
+
+module.exports.handle = async (event, context, callback) => {
+  if(event.httpMethod === "POST" && event.body){
+    const body = JSON.parse(event.body)
+    console.log(body)
+    await transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to: body.clientEmail,
+      replyTo: body.clientEmail, /*colocar email do cliente*/
+      subject: body.subject,
+      text: body.text,
+    })
+    .then(info => {
+      return callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(
+          {
+            message: 'Request Successful!',
+            input: info,
+          },
+        ),
+      })
+    })
+    .catch(err => {
+      return callback(null, {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(
+          {
+            message: 'Request not succeed!',
+            input: err
+          },
+        ),
+      })
+    })
+  }
+  else{
+    return callback(null, {
+      statusCode: 405,
+      body: JSON.stringify(
+        {
+          message: 'use POST method',
+        },
+      ),
+    })
+  }
+};
